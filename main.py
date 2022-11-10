027b5cdb4fbdcf92879310b9652be489b89f9d94
ANA LUIZA <110206256+AnaIncao@users.noreply.github.com>
2022-11-10 23:37:53 +0000

    aula14

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+
 import logging
 import traceback
+from flask import redirect
 
 import flask
 from replit import db
@@ -35,4 +37,16 @@ def limparBanco():
     logging.exception(e)
     return flask.render_template('contatos.html')
     
+
+@app.route('/limparResgistro/<email>', methods=['POST'])
+def limparResgistro(email):
+  try:
+    contatos = db.get('contatos', {});
+    del contatos[email];
+    db['contatos'] = contatos;
+    return redirect('/');
+  except Exception as e:
+    logging.exception(e);
+    return flask.render_template('contatos.html');
+  
 app.run('0.0.0.0')
